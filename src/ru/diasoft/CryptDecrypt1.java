24cca02f9703e58d498f7fd30c2457286d3ba31d
msa16 <mironovichsa@ya.ru>
2016-04-28 08:39:34 +0300

    CryptDecrypt1

--- src/ru/diasoft/CryptDecrypt1.java
+++ src/ru/diasoft/CryptDecrypt1.java
@@ -8,9 +8,9 @@ package ru.diasoft;
 import java.security.Key;
 import java.security.MessageDigest;
 import java.util.Arrays;
+import java.util.Base64;
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -26,6 +26,9 @@ public class CryptDecrypt1 {
         MessageDigest sha = MessageDigest.getInstance("SHA-1");
         key = sha.digest(key);
         key = Arrays.copyOf(key, 16); // use only first 128 bit
+        for (int i = 0; i < 16; i++) {
+            key[i] = (byte) (i + 1);
+        }
         SecretKeySpec secretKeySpec = new SecretKeySpec(key, ALGORITHM);
         return secretKeySpec;
     }
@@ -36,17 +39,19 @@ public class CryptDecrypt1 {
 
         Cipher aesCipher;
 
-        // Create the cipher
+        // Create the cipher AES/ECB/PKCS5Padding AES/CBC/NoPadding
         aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         //We use the generated aesKey from above to initialize the Cipher object for encryption
         // Initialize the cipher for encryption
         aesCipher.init(Cipher.ENCRYPT_MODE, generateKey());
-
         // Our cleartext
-        byte[] cleartext = "This is just an example2".getBytes();
-
+        byte[] cleartext = "1234567890123456".getBytes("utf-8");
         // Encrypt the cleartext
         byte[] ciphertext = aesCipher.doFinal(cleartext);
+
+        for (int i = 0; i < ciphertext.length; i++) {
+            System.out.println(ciphertext[i]);
+        }
 
         // Initialize the same cipher for decryption
         aesCipher.init(Cipher.DECRYPT_MODE, generateKey());
@@ -60,8 +65,53 @@ public class CryptDecrypt1 {
         // byte [] decryptedValue64 = new BASE64Decoder().decodeBuffer(value);
     }
 
+   public static byte[] encrypt(String key, String initVector, String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            return encrypted;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String key, String initVector, byte[] encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] original = cipher.doFinal(encrypted);
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }    
+    
+    private void test2() {
+        String key = "Bar12345Bar12345"; // 128 bit key
+        String initVector = "RandomInitVector"; // 16 bytes IV
+
+        byte[] enc = encrypt(key, initVector, "Hello World");
+        for (int i = 0; i < enc.length; i++) {
+            System.out.println(enc[i]);
+        }
+        System.out.println(decrypt(key, initVector, enc));
+    }
+    
     public static void main(String[] args) throws Exception {
-        new CryptDecrypt1().test1();
+//        new CryptDecrypt1().test1();
+        new CryptDecrypt1().test2();
     }
 
 }
